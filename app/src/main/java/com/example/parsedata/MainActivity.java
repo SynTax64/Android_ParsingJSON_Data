9adc54510d104765b32df232f2d9002eb36a1905
David A <davidaster007@gmail.com>
2021-09-08 11:13:26 +0200

    adding the final Project (Parsing Data from JSONArrays and JSONObject)

---
+++ app/src/main/java/com/example/parsedata/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.parsedata;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    String url = "https://jsonplaceholder.typicode.com/todos";
+    String getApiurl = "https://jsonplaceholder.typicode.com/todos/1";
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+//        JsonArrayRequest jsonArrayRequest = getJsonArrayRequest();
+//        queue.add(jsonArrayRequest);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                getApiurl, null, response -> {
+            try {
+                Log.d("jsonObj", "onCreate: \n" + response.getInt("userId") + "\n"
+                        + response.getInt("id") + "\n"
+                        + response.getString("title") + "\n"
+                        + response.getBoolean("completed") + "\n");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            Log.d("jsonObj", "onCreate: Failed!");
+        });
+
+//        getString(queue);
+
+        queue.add(jsonObjectRequest);
+    }
+
+    @NonNull
+    private JsonArrayRequest getJsonArrayRequest() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject jsonObject = response.getJSONObject(i);
+                    Log.d("JSON", "onCreate: " + jsonObject.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, error -> {
+            Log.d("JSON", "onCreate: Failed");
+        });
+        return jsonArrayRequest;
+    }
+
+    private void getString(RequestQueue queue) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            Log.d("Main", "onCreate: " + response.substring(0, 500));
+        },
+                error -> {
+                    Log.d("Main", "Failed to get info! ");
+                });
+        queue.add(stringRequest);
+    }
+}
